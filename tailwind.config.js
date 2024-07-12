c02ef6a2014cb330bf87755a16eb3693bf9e6cb4
ogasaawra <toma@ogasaawranoMac-mini.local>
2024-07-12 15:06:21 +0900

    TOP-header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,14 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {},
+    backgroundImage: {
+      "header-bg": "url('header_bg.png')",
+      "main-bg": "url('main_bg.png')",
+      "footer-bg": "url('footer_bg.png')",
+    }
+  },
+  plugins: [require("daisyui")],
 }
