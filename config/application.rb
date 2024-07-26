20aa5f828a5b08db5be34c171c0e74e90da4adb3
ogasaawra <toma@ogasaawranoMac-mini.local>
2024-07-27 04:05:10 +0900

    Herokuプッシュ時デバッグ

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module Myapp
 
     config.active_job.queue_adapter = :sidekiq
 
+    config.time_zone = 'Tokyo'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
