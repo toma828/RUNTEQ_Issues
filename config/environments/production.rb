db334e30f58ef7c824b4a71fc1fa3e236ff1d8bc
ogasaawra <toma@ogasaawranoMac-mini.local>
2024-07-28 04:12:49 +0900

    Herokuでのメール認証設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,5 +96,16 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  config.action_mailer.default_url_options = { host: 'yourdomain.com' }
+  config.action_mailer.default_url_options = { host: 'runteqdialy-1f1f8840ec21.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'herokuapp.com',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
