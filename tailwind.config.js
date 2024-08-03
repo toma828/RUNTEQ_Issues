36cf842df6778ea383c24e1f642802190bc26ba5
ogasaawra <toma@ogasaawranoMac-mini.local>
2024-08-04 02:48:33 +0900

    Showレイアウト変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     './app/javascript/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-red': '#ff9999', // 淡い赤色
+        'light-blue': '#a0d6ff', // 淡い青色
+      },
+    },
     backgroundImage: {
       "header-bg": "url('header_bg.png')",
       "main-bg": "url('main_bg.png')",
