1ca2addf0d11961762e7267760ff1043905578a9
ogasaawra <toma@ogasaawranoMac-mini.local>
2024-08-21 03:25:23 +0900

    フラッシュアニメーションの実装

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,17 @@ module.exports = {
         },
         'light-gray': '#8C6D5C',
       },
+      keyframes: {
+        flashFade: {
+          "0%": { transform: "translateX(180px)", opacity: 0 },
+          "20%": { transform: "translateX(0)", opacity: 1 },
+          "80%": { transform: "translateX(0)", opacity: 1 },
+          "100%": { transform: "translateX(180px)", opacity: 0 },
+        },
+      },
+      animation: {
+        flash: "flashFade 7.0s forwards",
+      },
     },
     backgroundImage: {
       "header-bg": "url('header_bg.png')",
