074a9fae56f58148bdf61aacaccd733360edd65b
ogasaawra <toma@ogasaawranoMac-mini.local>
2024-10-02 04:12:12 +0900

    Diary_index_refacta

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -5,7 +5,7 @@ class DiariesController < ApplicationController
     @selected_date = params[:selected_date] ? Date.parse(params[:selected_date]) : Date.today
     @start_date = @selected_date.beginning_of_week(:sunday)
     @end_date = @start_date + 6.days
-    @diaries = Diary.where(date: @start_date..@end_date).index_by(&:date)
+    @diaries = current_user.diaries.where(date: @start_date..@end_date).index_by(&:date)
   end
 
   def show
