7f7d2ec7b15f83ad8b4c418bc02de7cb013f5085
ogasaawra <toma@ogasaawranoMac-mini.local>
2024-10-03 03:43:07 +0900

    Add_user_destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = current_user
+    @user.destroy
+    redirect_to top_path, notice: 'ユーザーが削除されました。'
+  end
+
   private
 
   def user_params
